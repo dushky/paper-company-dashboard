bbb5ed41ac7e7fee930db41d38ec664abbab6bc7
dushky <dusanbako100@gmail.com>
2023-09-15 04:39:43 +0200

    style statistics

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,15 +1,23 @@
 import React from "react";
+import "./Statistics.css";
 import products from "../../data/products.json";
 import PieChart from "../UI/PieChart";
 import MostPopularCard from "../UI/MostPopularCard";
 import TotalRevenueCard from "../UI/TotalRevenueCard";
 
-const mostPopular = products.reduce((max, current) => {
+const mostPopularProduct = products.reduce((max, current) => {
   if (current.amount_sold > max.amount_sold) {
     return current;
   } else {
     return max;
   }
+});
+
+const mostRevenueProduct = products.reduce((max, current) => {
+  const currentRevenue = current.amount_sold * current.price;
+  const maxRevenue = max.amount_sold * max.price;
+
+  return currentRevenue > maxRevenue ? current : max;
 });
 
 let totalSold = 0;
@@ -21,16 +29,29 @@ products.map((product) => {
 
 const Statistics = () => {
   return (
-    <>
-      <PieChart
-        data={products.map((product) => {
-          return { label: product.name, value: product.amount_sold };
-        })}
-        centerLabel={`Total sold ${totalSold} pcs`}
-      />
-      <MostPopularCard title={"Most Popular"} data={mostPopular} />
-      <TotalRevenueCard title={"Total Revenue"} data={{sold: totalSold, revenue: totalRevenue}} />
-    </>
+    <div className="statistics">
+      <div className="pie-chart">
+        <PieChart
+          data={products.map((product) => {
+            return { label: product.name, value: product.amount_sold };
+          })}
+          centerLabel={`Total sold ${totalSold} pcs`}
+        />
+      </div>
+
+      <div className="card">
+        <MostPopularCard title={"Most Popular"} data={mostPopularProduct} />
+      </div>
+      <div className="card">
+        <TotalRevenueCard
+          title={"Total Revenue"}
+          data={{ sold: totalSold, revenue: totalRevenue }}
+        />
+      </div>
+      <div className="card">
+        <MostPopularCard title={"Most Revenue"} data={mostRevenueProduct} />
+      </div>
+    </div>
   );
 };
 
--- src/components/UI/MostPopularCard.tsx
+++ src/components/UI/MostPopularCard.tsx
@@ -4,34 +4,32 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
-
-
-
-export default function OutlinedCard({title, data}) {
+export default function OutlinedCard({ title, data }) {
   const card = (
     <React.Fragment>
       <CardContent>
-      <Typography sx={{ fontSize: 16 }} color="text.primary" gutterBottom>
-      {title}
-      </Typography>
+        <Typography sx={{ fontSize: 16 }} color="text.primary" gutterBottom>
+          {title}
+        </Typography>
         <Typography variant="h5" component="div">
           {data.name}
         </Typography>
 
-        <Typography variant="body2">
-          Total sold: {data.amount_sold} pcs
+        <Typography variant="body1">
+          sold: {data.amount_sold} pcs
         </Typography>
 
-        <Typography variant="body2">
+        <Typography variant="body1">
           Revenue: {data.amount_sold * data.price}€
         </Typography>
-
       </CardContent>
     </React.Fragment>
   );
   return (
-    <Box sx={{ minWidth: 275, maxWidth: 275 }}>
-      <Card variant="outlined">{card}</Card>
+    <Box sx={{ width: 160, height: 140 }}>
+      <Card variant="outlined" sx={{ width: "100%", height: "100%" }}>
+        {card}
+      </Card>
     </Box>
   );
 }
